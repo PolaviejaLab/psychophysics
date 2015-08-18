b4b2eca7fb43c0c950c4c7f1a8716502602d8273
Ivar Clemens <post@ivarclemens.nl>
2015-08-18 14:20:50 +0100

    Fixed spelling mistake

--- scripts/plab-ldap/step_4_list_users.sh
+++ scripts/plab-ldap/step_4_list_users.sh
@@ -7,5 +7,5 @@ klist
 echo ""
 echo "Attempting to download users database..."
 
-dapsearch -l 2 -H ldap://champalimaud.pt -b ou=UsersNeurociencias,dc=champalimaud,dc=pt > ../../data/users.ldif
+ldapsearch -l 2 -H ldap://champalimaud.pt -b ou=UsersNeurociencias,dc=champalimaud,dc=pt > ../../data/users.ldif
 kdestroy
